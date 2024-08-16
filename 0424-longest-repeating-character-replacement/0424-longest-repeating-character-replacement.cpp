403971445b740d4e4befd632b7b63feb78f43d49
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2024-08-16 22:41:20 +0530

    Time: 3 ms (95.92%), Space: 8.3 MB (72.56%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,19 +3,20 @@ public:
     int characterReplacement(string s, int k) {
         int ans=INT_MIN;
         int n = s.length();
-        for(int i=0;i<n;i++){
-            vector<int>hash(26,0);
-            int maxf=0;
-            for(int j=i;j<n;j++){
-                hash[s[j]-'A']++;
-                maxf = max(maxf,hash[s[j]-'A']);
-                int changes = (j-i+1) - maxf;
-                if(changes<=k){
-                    ans = max(ans,j-i+1);
-                }
-                else
-                    break;
+        int l=0;
+        int r=0;
+        int maxf=0;
+        vector<int>hash(26,0);
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxf = max(maxf,hash[s[r]-'A']);
+            while(l<r && (r-l+1) - maxf > k){
+                hash[s[l]-'A']--;
+                maxf = max(maxf,hash[s[l]-'A']);
+                l++;
             }
+            ans = max(ans,r-l+1);
+            r++;
         }
         return ans;
     }
