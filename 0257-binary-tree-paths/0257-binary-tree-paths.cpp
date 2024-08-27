69abbc77fa78b683da77efda8adeb2acaec30883
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2024-08-27 22:42:25 +0530

    Time: 4 ms (50.25%), Space: 17.1 MB (48.5%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,28 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void helper(TreeNode* root, string &temp, vector<string>& ans) {
-        if (root == NULL)
+    void helper(TreeNode* root,string temp,vector<string>&ans){
+        if(root==NULL)
             return;
-
-        if (root->left == NULL && root->right == NULL) {
-            temp += to_string(root->val);  // Add the leaf node value
-            ans.push_back(temp);  // Add the complete path to the result
-            temp.erase(temp.end() - to_string(root->val).length(), temp.end());  // Remove the leaf node value
-            return;
+        if(root->left==NULL && root->right==NULL){
+            temp+=to_string(root->val);
+            ans.push_back(temp);
+            return ;
         }
-
-        temp += to_string(root->val) + "->";  // Add the current node value and "->"
-        helper(root->left, temp, ans);  // Recur to the left child
-        helper(root->right, temp, ans);  // Recur to the right child
-
-        // Backtrack by removing the current node value and the arrow "->"
-        temp.erase(temp.end() - (to_string(root->val).length() + 2), temp.end());
+        temp+=to_string(root->val)+"->";
+        helper(root->left,temp,ans);
+        helper(root->right,temp,ans);
+        
     }
-
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> ans;
-        string temp = "";
-        helper(root, temp, ans);
+        vector<string>ans;
+        string temp="";
+        helper(root,temp,ans);
         return ans;
     }
 };
