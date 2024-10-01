47f40ab70b11169e11ca15dced12e388a0ec6487
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2024-10-01 20:42:46 +0530

    Time: 299 ms (69.22%), Space: 9.9 MB (76.15%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    bool solve(int i,int j,int indx,int n,int m,vector<vector<char>>& board,string &word, vector<vector<int>>&visited){
-        if(indx==word.length())
-            return true;
-        
-        if(i<0 || j<0 || i>=n || j>=m || visited[i][j] || board[i][j] != word[indx])
+    bool solve(int i, int j, int n, int m, vector<vector<char>>& board, string& temp, string& word, vector<vector<int>>& visited) {
+        if (temp.length() > word.length()) return false; 
+        if (temp == word) return true;
+        if (i < 0 || j < 0 || i >= n || j >= m || visited[i][j] || board[i][j] != word[temp.length()]) 
             return false;
-        visited[i][j]=true;
-        bool left=false,right=false,up=false,down=false;
-        up = solve(i-1,j,indx+1,n,m,board,word,visited);
-        down = solve(i+1,j,indx+1,n,m,board,word,visited);
-        left = solve(i,j-1,indx+1,n,m,board,word,visited);
-        right = solve(i,j+1,indx+1,n,m,board,word,visited);     
-        visited[i][j]=false;
+        visited[i][j] = true;
+        temp.push_back(board[i][j]);
+
+        bool up = solve(i - 1, j, n, m, board, temp, word, visited);    // Move up
+        bool down = solve(i + 1, j, n, m, board, temp, word, visited);  // Move down
+        bool left = solve(i, j - 1, n, m, board, temp, word, visited);  // Move left
+        bool right = solve(i, j + 1, n, m, board, temp, word, visited); // Move right
+
+        temp.pop_back();
+        visited[i][j] = false;
+
         return up || down || left || right;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size();
-        int m=board[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,false));
-        string temp="";
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(solve(i,j,0,n,m,board,word,visited))
-                    return true;
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, false));
+        string temp = "";
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (solve(i, j, n, m, board, temp, word, visited)) {
+                    return true; 
+                }
             }
         }
-         return false;
+
+        return false;  
     }
 };
