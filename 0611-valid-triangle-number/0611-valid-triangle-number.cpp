b7c499a08e18d36353e698725905afdf72a172f8
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2024-10-02 07:16:01 +0530

    Time: 149 ms (32.9%), Space: 16 MB (19.17%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -6,10 +6,18 @@ public:
         int ans=0;
         for(int i=0;i<n-2;i++){
             for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]+nums[j]>nums[k])
-                        ans++;
+                int low=j+1;
+                int high=n-1;
+                while(low<=high){
+                    int mid = low+(high-low)/2;
+                    if(nums[mid]>=nums[i]+nums[j]){
+                        high=mid-1;
+                    }
+                    else {
+                        low=mid+1;
+                    }
                 }
+                ans+=low-j-1;
             }
         }
         return ans;
