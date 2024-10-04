85099f83876bc1c1c49fd1ded9feb9504209292c
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2024-10-04 13:08:55 +0530

    Time: 36 ms (56.14%), Space: 24.2 MB (18.87%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -3,23 +3,20 @@ public:
     int maxPoints(vector<vector<int>>& points) {
         if(points.size()==1)
             return 1;
-        int maxi=0;
+        int result=0;
         for(int i=0;i<points.size();i++){
-            for(int j=i+1;j<points.size();j++){
-                int x = (points[i][0]-points[j][0]);
-                int y = (points[i][1]-points[j][1]);
-                int count=2;
-                for(int k=0;k<points.size();k++){
-                    if(k!=i && k!=j){
-                        int x_=  (points[i][0] - points[k][0]);
-                        int y_ = (points[i][1] - points[k][1]);
-                        if(x*y_ == y*x_)
-                            count++;
-                    }
+            unordered_map<double,int>map;
+            int maxi=0;
+            for(int j=0;j<points.size();j++){
+                if(i==j) continue;
+                int x = (points[j][0]-points[i][0]);
+                int y = (points[j][1]-points[i][1]);
+                double value = atan2(y,x);
+                map[value]++;
+                maxi=max(maxi,map[value]);
                 }
-                maxi=max(maxi,count);
-            }
+            result=max(result,maxi+1);
         }
-        return maxi;
+        return result;
     }
 };
