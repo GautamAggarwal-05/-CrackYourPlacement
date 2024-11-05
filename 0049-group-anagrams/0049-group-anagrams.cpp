1da6c098e6c52b9618a410239ca1839a1229faac
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2024-11-05 19:59:35 +0530

    Time: 15 ms (81.73%), Space: 22.7 MB (95.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,int>mp;
+        unordered_map<string,int>map;
         vector<vector<string>>res;
-
-        for(int i=0;i<strs.size();i++)
-        {
-            string temp=strs[i];
+        for(int itr=0;itr<strs.size();itr++){
+            string temp=strs[itr];
             sort(temp.begin(),temp.end());
-            if(mp.find(temp)!=mp.end())
-            {
-                res[mp[temp]].push_back(strs[i]);
+            if(map.find(temp)!=map.end()){
+                res[map[temp]].push_back(strs[itr]);
             }
-            else
-            {
-                mp[temp]=res.size();
-                res.push_back({strs[i]});
+            else{
+                map[temp]=res.size();
+                res.push_back({strs[itr]});
             }
         }
         return res;
     }
 };
-
