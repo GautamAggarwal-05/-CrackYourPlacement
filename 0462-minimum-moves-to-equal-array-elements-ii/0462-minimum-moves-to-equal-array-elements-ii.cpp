fcbfd0905e64accbf7304a6cd50d0c0afa406b96
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-01-02 19:50:57 +0530

    Time: 4 ms (16.67%), Space: 15.6 MB (45.8%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
+        sort(nums.begin(),nums.end());
         int elem = nums[n/2];
         int ans=0;
         for(int i=0;i<n;i++){
-            ans += abs(elem - nums[i]);
+            ans += abs(elem-nums[i]);
         }
         return ans;
     }
