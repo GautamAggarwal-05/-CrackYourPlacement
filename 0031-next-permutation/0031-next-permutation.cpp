78eacb006158533fc0706f445767012f8f1b13f8
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-06-21 23:11:18 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.5%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,22 +1,20 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        
-        int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1])
-            i--;
-
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i])
-                j--;
-            swap(nums[i], nums[j]);
+        int n=nums.size();
+        int i=n-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+                    i--;
         }
-        reverse(nums.begin() + i + 1, nums.end());
+        cout<<i;
+        int j=n-1;
+
+        while(i>=0 &&j>i && nums[i]>=nums[j])
+            j--;
+        cout<<j;
+        if(i>=0)
+            swap(nums[i],nums[j]);
+        reverse(nums.begin()+i+1,nums.end());
+        return;
     }
 };
