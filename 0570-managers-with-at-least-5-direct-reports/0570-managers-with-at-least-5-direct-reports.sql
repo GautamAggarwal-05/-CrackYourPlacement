7d173b3710502a83c261c0d8a0a5eb3158fcb140
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-07-14 16:19:31 +0530

    Time: 373 ms (27.35%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,5 @@
-select name
-from employee
-where id in (select managerid from employee Group by managerid having count(*)>=5);
+select e1.name
+from employee e1
+join employee e2 on e1.id = e2.managerId
+group by e1.id
+having count(e1.id)>=5;
