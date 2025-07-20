833ec60ea419d540c2647a512db63a31cfddc345
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-07-20 17:01:36 +0530

    Time: 432 ms (43.46%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 select max(m1.num) as num
-from MyNumbers as m1
-join (select m.num as num
+from (select m.num as num
 from MyNumbers as m
 group by m.num
-having count(m.num)=1) as m2
-on m2.num = m1.num;
+having count(m.num)=1) AS m1
