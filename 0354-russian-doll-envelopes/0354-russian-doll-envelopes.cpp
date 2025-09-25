12c38a68a073d5af82731a2314acab93703dab28
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-09-25 16:07:10 +0530

    Time: 48 ms (40.96%), Space: 81.8 MB (61.68%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-     static bool comp(pair<int,int> &a,pair<int,int>&b){
-        if(a.first==b.first){
-            return a.second>b.second;
+    static bool comp(vector<int> &a,vector<int>&b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
         }
         else{
-            return a.first<b.first;
+            return a[0]<b[0];
         }
     }
-
-    int maxEnvelopes(vector<vector<int>>& env) {
-        vector<pair<int,int>>arr(env.size());
-        for(int i = 0 ; i < env.size() ; i++){
-            arr[i] = {env[i][0],env[i][1]};
-        }
-
-        sort(arr.begin(),arr.end(),comp);
-        
-        vector<int>ans;
-        ans.push_back(arr[0].second);
-        for(int i=1;i<env.size();i++)
-        {
-            if(arr[i].second > ans.back())
-                ans.push_back(arr[i].second);
-            else
-            {
-                int indx = lower_bound(ans.begin(),ans.end(),arr[i].second)-ans.begin();
-                ans[indx]=arr[i].second;
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(),envelopes.end(),comp);
+        vector<int> temp;
+        temp.push_back(envelopes[0][1]);
+        for(int i=1;i<envelopes.size();i++){
+            if(temp.back()<envelopes[i][1]){
+                temp.push_back(envelopes[i][1]);
+            }
+            else{
+                auto ind=lower_bound(temp.begin(),temp.end(),envelopes[i][1])-temp.begin();
+                temp[ind]=envelopes[i][1];
             }
         }
-        return ans.size();
+        return temp.size();
     }
 };
