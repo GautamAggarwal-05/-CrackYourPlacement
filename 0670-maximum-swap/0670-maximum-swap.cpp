324d0fe864acb63cb6d0d00c228bb167aa5c42d1
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-10-15 09:35:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (88.88%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -3,24 +3,22 @@ public:
     int maximumSwap(int num) {
         string str = to_string(num);
         int n=str.length();
-        vector<int>maxRightIndex(n);// store the index of maximum value present in right of the curr value
-        maxRightIndex[n-1]=n-1;
-        // char maxi=str[n-1];
+        int maxdigitindex=n-1;
+        int swap1=-1;
+        int swap2=-1;
         for(int i=n-2;i>=0;i--){
-            if(str[i]>str[maxRightIndex[i+1]]){
-                maxRightIndex[i]=i;
-            }else{
-                maxRightIndex[i]=maxRightIndex[i+1];
+            if(str[i]<str[maxdigitindex]){
+                swap1=i;
+                swap2=maxdigitindex;
+            }else if(str[i]>str[maxdigitindex]){
+                maxdigitindex=i;
             }
         }
-
-        for(int i=0;i<n;i++){
-            if(str[i]<str[maxRightIndex[i]]){
-                swap(str[i],str[maxRightIndex[i]]);
-                break;
-            }
-        }
-        int ans=stoi(str);
+        if(swap1!=-1 && swap2!=-1)
+            swap(str[swap1],str[swap2]);
+        
+        int ans = stoi(str);
         return ans;
     }
+
 };
