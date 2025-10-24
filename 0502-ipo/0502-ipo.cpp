72b3371d16989c69d3e0d690aa224c644d4f8a45
Gautam Aggarwal <143955565+GautamAggarwal-05@users.noreply.github.com>
2025-10-24 20:04:40 +0530

    Time: 167 ms (10.96%), Space: 85.5 MB (100%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        priority_queue<int>maxheap;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>minheap;
+        // Min-heap to keep track of projects based on their capital requirement
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minCapitalPQ;
+        // Max-heap to keep track of the available projects based on their profits
+        priority_queue<int> maxProfitPQ;
 
-        for(int i=0;i<capital.size();i++){
-            minheap.push({capital[i],profits[i]});
+        // Push all projects into the min-heap
+        for (int i = 0; i < capital.size(); ++i) {
+            minCapitalPQ.push({capital[i], profits[i]});
         }
 
-        while(k>0){
-
-            while(!minheap.empty() && minheap.top().first <= w){
-                maxheap.push(minheap.top().second);
-                minheap.pop();
+        while (k > 0) {
+            // Move all projects that can be started with the current capital to the max-heap
+            while (!minCapitalPQ.empty() && minCapitalPQ.top().first <= w) {
+                maxProfitPQ.push(minCapitalPQ.top().second);
+                minCapitalPQ.pop();
             }
-            if(maxheap.empty()){
+
+            // If no projects can be started, return the current capital
+            if (maxProfitPQ.empty()) {
                 return w;
             }
-            w+=maxheap.top();
-            maxheap.pop();
+
+            // Start the project with the maximum profit
+            w += maxProfitPQ.top();
+            maxProfitPQ.pop();
+
             k--;
         }
+
         return w;
     }
 };
